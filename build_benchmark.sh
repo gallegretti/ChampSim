4ff6fc21dbcafbea44e1dc4ff1f2d33fedba0931
gallegretti <gallegretti@inf.ufrgs.br>
2019-12-24 16:32:52 -0300

    Add build benchmark script

---
+++ build_benchmark.sh
@@ -0,0 +1,3 @@
+./build_champsim.sh bimodal no no no no lru 1
+./build_champsim.sh bimodal next_line next_line ip_stride no lru 1
+./build_champsim.sh hashed_perceptron next_line next_line kpcp next_line drrip 1

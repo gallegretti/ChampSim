21bf8cf35f5b71d562e133542fca495babe4a605
Gabriel Allegretti <gallegretti@inf.ufrgs.br>
2020-01-01 15:14:41 -0300

    Add more plots and improve script

--- plot.py
+++ plot.py
@@ -19,8 +19,13 @@ def readFile(filePath):
 
         # Specific for prefetcher
         iptSize = re.findall(r'ipt_(\d+)', filePath)
-        if (iptSize):
+        if (iptSize != None and len(iptSize) > 0):
             dic['iptSize'] = int(iptSize[0])
+
+        nway = re.findall(r'(\d+)_way', filePath)
+        if (nway != None and len(nway) > 0):
+            dic['nway'] = int(nway[0])
+
         return dic
 
 def genericPlot(barWidth, allBars, allColors, allLabels, xlabel):    
@@ -37,23 +42,23 @@ def genericPlot(barWidth, allBars, allColors, allLabels, xlabel):
     plt.yscale('log')
     # Add xticks on the middle of the group bars
     plt.xlabel(xlabel, fontweight='bold')
-    plt.xticks([r + barWidth for r in range(len(allBars[0]))], ['A', 'B', 'C', 'D', 'E'])
+    plt.xticks([], [])
 
     # Create legend & Show graphic
     plt.legend()
     plt.show()
 
-def prefetcherPlot(data):
+def prefetcherPlot(data, key, labelfn):
     # Prefetcher
-    iptSizes = sorted(set(map(lambda x : x['iptSize'], data)))
+    iptSizes = sorted(set(map(lambda x : x[key], data)))
     traceFiles = list(set(map(lambda x : x['traceFile'], data)))
-    labels = list(map(lambda x : str(x) + ' entries', iptSizes))
+    labels = list(map(labelfn, iptSizes))
 
     iptBars = []
     for size in iptSizes:
         iptBars.append([])
         for trace in traceFiles:
-            iptBars[len(iptBars) - 1].append(list(filter(lambda x : x['iptSize'] == size and x['traceFile'] == trace, data))[0]['totalCycles'])
+            iptBars[len(iptBars) - 1].append(list(filter(lambda x : x[key] == size and x['traceFile'] == trace, data))[0]['totalCycles'])
 
     barWidth = 0.25
     allColors = [
@@ -85,8 +90,10 @@ def main():
         filePath = path + '/' + file
         filesContent.append(readFile(filePath))
 
-    if (plotType == 'prefetcher'):
-        prefetcherPlot(filesContent)
+    if (plotType == 'prefetcher_ipt'):
+        prefetcherPlot(filesContent, 'iptSize', lambda x : str(x) + ' entries')
+    elif (plotType == 'prefetcher_nway'):
+        prefetcherPlot(filesContent, 'nway', lambda x : str(x) + '-way')
     else:
         # Add plotters here
         print('Unknown plotter')

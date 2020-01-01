89785da04f8bb63cd59a1af3c7040e6a5f92bfc3
azhow <arthur.zachow@gmail.com>
2020-01-01 19:05:27 -0300

    all cache replacement are working

--- inc/cache.h
+++ inc/cache.h
@@ -227,7 +227,7 @@ class CACHE : public MEMORY {
              mru_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK *current_set, uint64_t ip, uint64_t full_addr, uint32_t type);
 
 	private:
-		static std::queue<int> m_fifo();
+		static std::queue<int> m_fifo;
 };
 
 #endif
